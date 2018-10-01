3ec8ed8b97e9c944b689ed51611d3d83be7566b0
Chitrang Natu <chit786@gmail.com>
2018-10-01 19:15:57 +0200

    selenoid support

---
+++ scripts/selenoid-start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run -d                                   \
+--name selenoid                                 \
+-p 4444:4444                                    \
+-v /var/run/docker.sock:/var/run/docker.sock    \
+-v `pwd`/config/:/etc/selenoid/:ro              \
+aerokube/selenoid:latest-release
---
+++ scripts/selenoid-stop.bat
@@ -0,0 +1,2 @@
+docker stop selenoid
+docker rm selenoid
